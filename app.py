895615976c2e68e3e7a6add6feb7d1fb33e70f4a
aditya0by0 <aditya0by0@gmail.com>
2023-05-12 12:29:39 +0200

    Commit Week 2  - Pycoders
    Implement
    - backend code for searching
    - backend code for the product details page
    - Modified daolayer
    - working more login and filter (&dropdown menu)

--- app.py
+++ app.py
@@ -1,44 +1,30 @@
 from flask import Flask, render_template, request
 from daolayer.SQLReadWrite import SQLReadWrite
+import auth
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.register_blueprint(auth.bp)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def get_home_page():
 	return render_template("home.html")
 
-@app.route("/product")
-def get_product_page():
-	return render_template("product_details.html")
+@app.route("/product/<p_id>", methods=['POST'])
+def get_product_page(p_id):
+	with SQLReadWrite.engine.connect() as conn:
+		result = conn.execute('SELECT * from products where pid=%s', (p_id,))
+	result_dict = [dict(row) for row in result.all()][0]
+	return render_template("products/productDetails.html",product = result_dict)
 
-@app.route("/signup", methods=['GET'])
-def signup_get():
-	return render_template("signupPage.html")
+@app.route("/search", methods=['POST'])
+def search():
+	searched = request.form['searched']
+	with SQLReadWrite.engine.connect() as conn:
+		result = conn.execute('SELECT * from products where category LIKE %s',
+								('%'+searched+'%',))
+	result_dict = [dict(row) for row in result.all()]
+	return render_template('search.html', searched=searched , products = result_dict)
 
-@app.route("/signup", methods=['POST'])
-def signup_post():
-	
-	name = request.form['name']
-	email = request.form['email']
-	password = request.form['password']
-
-	if SQLReadWrite.check_if_exists('users', field='email', operator='=', value=email ):
-		return 'User already exist with registered email id'
-
-	if SQLReadWrite.put_data('users', name=name, email=email, password=password):
-		return " registered sucessfully"
-	else:
-		return " An error occured" 
-
-
-
-@app.route("/login")
-def login():
-	return render_template("loginPage.html")
-
-@app.route("/test")
-def test():
-	return render_template("test.html")
 
 
 if __name__ == "__main__":
--- daolayer/SQLReadWrite.py
+++ daolayer/SQLReadWrite.py
@@ -10,58 +10,7 @@ class SQLReadWrite:
 	engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}/{database}')
 
 	@staticmethod
-	def put_data(table_name, **kwargs):
-
-		try:
-			string = "insert into " + table_name + str(tuple(kwargs.keys())) + \
-			 		 ' values ' + str(tuple(kwargs.values()))
-
-			print(string)
-
-			with SQLReadWrite.engine.connect() as conn:
-				result = conn.execute(text(string))
-		except Exception as e:
-			# raise "Date Insert operation failed"
-			return False
-		else:
-			return True
-
-		
-	@staticmethod
-	def check_if_exists(table_name, **kwargs):
-		
-		list_ = SQLReadWrite.get_data(table_name, **kwargs)
-		if len(list_) != 0:
-			return True
-		
-		return False 
-
-	@staticmethod
-	def get_data(table_name, **kwargs):
-		result_dict=[]
-		string = "select * from " + table_name
-
-		if kwargs :
-			string += ' where'
-			for key in ['field', 'operator', 'value']:
-				if key not in kwargs: 
-					raise 'All parameters not supplied'
-				if key == 'value' :
-					string += " '" + kwargs[key] + "'"
-					continue
-				string += ' ' + kwargs[key]
-
-		print(string)
-				 
+	def execute_query(query, ptuple=()):
 		with SQLReadWrite.engine.connect() as conn:
-			result = conn.execute(text(string))
-		
-		return [dict(row) for row in result.all()]
-
-	@staticmethod
-	def read_data():
-		pass
-
-	@staticmethod
-	def delete_data():
-		pass
+			result = conn.execute(query, ptuple)
+		return result
